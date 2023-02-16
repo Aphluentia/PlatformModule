97c22dea7b41c05e8e173428ae0e1b18d8628039
Pedro Marques <pedroagoncalvesmarques@ua.pt>
2023-02-16 12:24:45 +0000

    Adds Base Controllers #1
    Closes #1

--- WebPlatform/WebPlatform/Program.cs
+++ WebPlatform/WebPlatform/Program.cs
@@ -11,6 +11,7 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+    
 }
 
 app.UseHttpsRedirection();
@@ -19,7 +20,6 @@ app.UseStaticFiles();
 app.UseRouting();
 
 app.UseAuthorization();
-
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
