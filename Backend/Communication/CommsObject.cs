905bb162dbd6c925da160cc6434354b2e5d17740
Pedro Marques <pedroagoncalvesmarques@ua.pt>
2023-02-16 16:01:18 +0000

    Adds Backend Project #1

---
+++ Backend/Communication/CommsObject.cs
@@ -0,0 +1,9 @@
+﻿namespace WebPlatform.Communication
+{
+    public class CommsObject
+    {
+        public string Topic { get; set; }
+        public string Action { get; set; }
+        public string Message { get; set; }
+    }
+}
